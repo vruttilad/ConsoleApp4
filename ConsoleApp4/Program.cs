35db5a85fbf94ad762e50f4ae16c1786e00bc3c3
vrutti lad <vruttilad2000@gmail.com>
2022-03-09 17:18:45 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartProgram start = new StartProgram();
+            start.activity += Start_activity;
+            start.StartProcess();
+        }
+
+        private static void Start_activity()
+        {
+            Console.WriteLine("hiiiiiii");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp4/StartProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    public delegate void start();
+    internal class StartProgram
+    {
+
+
+        public event start activity;
+        public void StartProcess()
+        {
+            Console.WriteLine("\n Process started");
+            Console.ReadLine();
+            OncompletedProcess();
+        }
+
+        private void OncompletedProcess()
+        {
+            activity?.Invoke();
+        }
+    }
+}
